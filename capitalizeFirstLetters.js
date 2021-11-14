26b8854a77c7a2d2d7d9463d9d423d87571d296b
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-14 15:47:55 +0100

    js-tdd-intro

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,11 @@
 
 const assert = require('assert');
 
+function capitalizeFirstLetters(input) {
+    return input.split(" ").map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }).join(" ");
+}
 
 
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
