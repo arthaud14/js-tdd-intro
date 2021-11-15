7b9119f15f59c3a9847e8a28d77013d6f5e298cc
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-15 11:16:01 +0100

    js-tdd-intro

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,3 @@
-
-const assert = require('assert');
-
 function capitalizeFirstLetters(input) {
     return input.split(" ").map(function(word) {
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
@@ -8,13 +5,7 @@ function capitalizeFirstLetters(input) {
 }
 
 
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-
-assert.strictEqual(capitalizeFirstLetters('javaScript is good'), 'Javascript Is Good');
-
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-
-assert.strictEqual(capitalizeFirstLetters(''), '');
+module.exports = capitalizeFirstLetters;
 
 
 
